e7d1058c6090c95cfd9dc1231ea70011796affe2
Dan Walton <danw@devinit.org>
2019-06-06 16:30:10 +0100

    Calculate VNR thresholds

--- project-code/P20 gaps.R
+++ project-code/P20 gaps.R
@@ -6,7 +6,7 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 #Query povcal at all thresholds for given countries and years; this formula is inefficient and can be optimised
-povcal.threshold.national <- function(targetHC,countries="all",year="all",lowerguess=0, upperguess=5,precision=0.01){
+povcal.threshold.national <- function(countries="all",year="all",lowerguess=0, upperguess=5,precision=0.01){
   data.list <- list()
   PLs <- seq(lowerguess,upperguess,by=precision)
   #pb <- txtProgressBar(min=0, max=length(PLs),style=3)
@@ -20,18 +20,30 @@ povcal.threshold.national <- function(targetHC,countries="all",year="all",lowerg
     }
   #close(pb)
   data <- rbindlist(data.list)
-  data$diff <- abs(targetHC-data$HeadCount)
-  datadt <- as.data.table(data)
-  datadt$CountryYear <- paste0(datadt$CountryName,datadt$CoverageType,datadt$RequestYear)
-  datadt <- datadt[datadt[, .I[diff == min(diff)], by=CountryYear]$V1]
-  datadt <- datadt[datadt[, .I[PovertyLine==min(PovertyLine)], by=CountryYear]$V1]
-  return(datadt)
+  return(data)
 }
+
 
 req.years <- c(1990,1993,1996,1999,2002,2005,2008,2010,2011,2012,2013,2015)
 
 #VNR countries, excluding those without PovcalNet data: Cambodia, Eritrea, Kuwait, Liechtenstein, Nauru, New Zealand, Oman and Palau 
-req.countries <- c("DZA","AZE","BIH","BFA","CMR","CAF","TCD","CHL","COG","CIV","HRV","SLV","SWZ","FJI","GHA","GTM","GUY","ISL","IDN","IRQ","ISR","KAZ","LSO","MRT","MUS","MNG","PAK","PHL","RWA","LCA","SRB","SLE","ZAF","TLS","TON","TUN","TUR","TKM","GBR","TZA","VUT")
+#req.countries <- c("DZA","AZE","BIH","BFA","CMR","CAF","TCD","CHL","COG","CIV","HRV","SLV","SWZ","FJI","GHA","GTM","GUY","ISL","IDN","IRQ","ISR","KAZ","LSO","MRT","MUS","MNG","PAK","PHL","RWA","LCA","SRB","SLE","ZAF","TLS","TON","TUN","TUR","TKM","GBR","TZA","VUT")
+req.countries <- "all"
 
 #This takes a LONG time to run
-P20.thresholds.national <- povcal.threshold.national(targetHC = 0.2, countries=req.countries, year=req.years, lowerguess=0, upperguess=45, precision=0.01)
+thresholds.national <- povcal.threshold.national(countries=req.countries, year=req.years, lowerguess=0, upperguess=45, precision=0.01)
+
+targetHC <- 0.2
+P20.thresholds.national <- thresholds.national
+P20.thresholds.national$diff <- abs(targetHC-P20.thresholds.national$HeadCount)
+P20.thresholds.national <- as.data.table(P20.thresholds.national)
+P20.thresholds.national <- P20.thresholds.national[P20.thresholds.national[, .I[diff == min(diff)], by=.(RequestYear,CountryName)]$V1]
+P20.thresholds.national <- P20.thresholds.national[P20.thresholds.national[, .I[PovertyLine==min(PovertyLine)], by=.(RequestYear,CountryName)]$V1]
+
+#Calculate mean consumptions and gaps
+P20.thresholds.national$p20.mean <- P20.thresholds.national$PovertyLine*(1-P20.thresholds.national$PovGap/P20.thresholds.national$HeadCount)
+P20.thresholds.national$non.p20.mean <- (P20.thresholds.national$Mean*12/365.25-P20.thresholds.national$p20.mean*P20.thresholds.national$Headcount)/(1-P20.thresholds.national$Headcount)
+P20.thresholds.national$p20.income.gap <- P20.thresholds.national$non.p20.mean - P20.thresholds.national$p20.mean
+
+#Calculate growth in income gap by country
+P20.gap.growth <- P20.thresholds.national[, gap.growth=coefficients(lm(p20.income.gap~RequestYear))[2],by=CountryName]
